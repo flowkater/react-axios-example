a915f93c53639abc5e80e9630d2d4e892cb53c1f
flowkater <chjw102@gmail.com>
2017-03-21 12:21:09 +0900

    Complete Counter

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,21 +1,62 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import { connect } from 'react-redux';
 import { receiveValue } from '../../actions';
 import style from './Counter.css'
 
+import { Spinner } from '../';
+
 class Counter extends Component {
+    constructor(props) {
+        super(props);
+        this.onClick = this.onClick.bind(this);
+    }
+
+    componentDidMount() {
+        let getNumber = () => {
+            axios.get('/counter').then(response => {
+                this.props.onReceive(response.data.number);
+                setTimeout(getNumber, 1000 * 5);
+            });
+        };
+
+        setTimeout(getNumber, 1000);
+    }
+
+    onClick(){
+        axios.post('/counter').then(response => {
+            this.props.onReceive(response.data.number);
+        });
+    }
+    
+
     render() {
+        const number = (
+            <div className={style.number} ref={ref => {this.element = ref}}> 
+                {this.props.value}
+            </div>
+        );
+
+        const spinner = (
+            <Spinner />
+        );
+
         return (
-            <div className={style.container}>
+            <div className={style.container} onClick={this.onClick}>
                 <div className={style.center}>
-                    <div className={style.number}> 
-                        {this.props.value}
-                    </div>
+                    { (this.props.value == -1) ? spinner : number }
                 </div>   
             </div>
         );
     }
+
+    componentDidUpdate(prevProps, prevState) {
+        this.element.classList.remove(style.bounce);
+        this.element.offsetWidth;
+        this.element.classList.add(style.bounce);
+    }
+    
 }
 
 const mapStateToProps = (state, ownProps) => {
--- src/components/Spinner/Spinner.js
+++ src/components/Spinner/Spinner.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import style from './Spinner.css';
 
 class Spinner extends Component {
     render() {
         return (
-            <div>
-                Spinner
+            <div className={style.spinner}>
+            <div className={style.rect1}></div>
+            <div className={style.rect2}></div>
+            <div className={style.rect3}></div>
+            <div className={style.rect4}></div>
+            <div className={style.rect5}></div>
             </div>
         );
     }
